a0392d01f845cdf2b47c66e5691d0d5f1045e938
Alexander Khrustalev <st.alex.ser@gmail.com>
2021-01-28 09:48:31 +0200

    Добавление WOW.js

--- app/js/app.js
+++ app/js/app.js
@@ -13,13 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 	document.head.appendChild(link);
 	// });
 	// //Добавление скриптов в html
-	// [ 'https://alex-khrust.github.io/S.Tomilo/app/js/app.js',
-	// ].forEach(function(src) {
-	// 	var script = document.createElement('script');
-	// 	script.src = src;
-	// 	script.async = false;
-	// 	document.body.appendChild(script);
-	// });
+	[ 'https://alex-khrust.github.io/S.Tomilo/app/js/libs.min.js',
+	].forEach(function(src) {
+		var script = document.createElement('script');
+		script.src = src;
+		script.async = false;
+		document.head.appendChild(script);
+	});
+	[ 'https://alex-khrust.github.io/S.Tomilo/app/js/app.js',
+	].forEach(function(src) {
+		var script = document.createElement('script');
+		script.src = src;
+		script.async = false;
+		document.body.appendChild(script);
+	});
 
 	// Скролл к id по клику на ссылку, и добавление ссылке класса aktive
 	$('a[href^="#"]').on('click', function (e) {
@@ -76,6 +83,32 @@ document.addEventListener('DOMContentLoaded', () => {
   $(".popup-overlay, .popup .close-btn").click(function () {
 		$(".popups, .popup").removeClass("show");
 		$('body').removeClass('locked');
-  });
+	});
+	
+
+	//Добавление классов и атрибутов для WOW анимации
+	var plus = 0;
+	$('.practice-list li').addClass('wow fadeInRight').each(function(i) {
+    plus += 0.15;
+    $(this).attr({'data-wow-delay': plus + 's' , 'data-wow-duration':'0.5s'});
+	});
+	
+	//WOW анимационные эффекты
+	var wow = new WOW(
+		{		
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       true,       // trigger animations on mobile devices (default is true)
+			live:         true,       // act on asynchronously loaded content (default is true)
+			callback:     function(box) {
+				// the callback is fired every time an animation is started
+				// the argument that is passed in is the DOM node being animated
+			},
+			scrollContainer: null,    // optional scroll container selector, otherwise use window,
+			resetAnimation: true,     // reset animation on end (default is true)
+		}
+	);
+	wow.init();
 
 })
