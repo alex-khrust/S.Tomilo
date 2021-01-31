df875c0d27ae974b3b2bd1c384cc4efafdedc43d
Alexander Khrustalev <st.alex.ser@gmail.com>
2021-01-31 20:33:04 +0200

    Добавление анимации и правка размера фигур декора

--- app/app.js
+++ app/app.js
@@ -1,5 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
+
 	// Custom JS
+
+	// //Добавление стилей в html
+	// [ 'https://alex-khrust.github.io/S.Tomilo/app/css/app.min.css',
+	// ].forEach(function(href) {
+	// 	var link = document.createElement('link');
+	// 	link.rel  = 'stylesheet';
+  //   link.type = 'text/css';
+	// 	link.href = href;
+	// 	link.async = false;
+	// 	document.head.appendChild(link);
+	// });
+	// //Добавление скриптов в html
+	[ 'https://alex-khrust.github.io/S.Tomilo/app/js/libs.min.js',
+	].forEach(function(src) {
+		var script = document.createElement('script');
+		script.src = src;
+		script.async = false;
+		document.head.appendChild(script);
+	});
+	// [ 'https://alex-khrust.github.io/S.Tomilo/app/js/app.js',
+	// ].forEach(function(src) {
+	// 	var script = document.createElement('script');
+	// 	script.src = src;
+	// 	script.async = false;
+	// 	document.body.appendChild(script);
+	// });
 
 	// Скролл к id по клику на ссылку, и добавление ссылке класса aktive
 	$('a[href^="#"]').on('click', function (e) {
